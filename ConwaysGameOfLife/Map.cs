1d97fd51b194175477eb462acd509600cf9b99f5
Mary Kergosien <mary@kergosien.net>
2015-11-11 10:35:50 -0600

    Initial Commit

---
+++ ConwaysGameOfLife/Map.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConwaysGameOfLife
+{
+    public class Map
+    {
+        private bool[,] currentBoard;
+        private bool[,] nextBoard;
+        public Map(bool[,] newBoard)
+        {
+            currentBoard = newBoard;
+            nextBoard = new bool[newBoard.GetLength(0), newBoard.GetLength(1)];
+        }
+
+        public void Tick()
+        {
+            for (int i = 0; i < currentBoard.GetLength(0); i++) //rows
+            {
+                for (int j = 0; j < currentBoard.GetLength(1); j++) //columns
+                {
+                    int alive = 0; //cells
+
+                    try
+                    {
+                        if (currentBoard[i - 1, j - 1]) //doesnt need ==true
+                        {
+                            alive++;
+                        }
+                    }
+                    catch { }
+
+                    try
+                    {
+                        if (currentBoard[i - 1, j] == true)
+                        {
+                            alive++;
+                        }
+                    }
+                    catch { }
+
+                    try
+                    {
+                        if (currentBoard[i - 1, j + 1] == true)
+                        {
+                            alive++;
+                        }
+                    }
+                    catch { }
+
+                    try
+                    {
+                        if (currentBoard[i, j + 1] == true)
+                        {
+                            alive++;
+                        }
+
+                    }
+                    catch { }
+
+                    try
+                    {
+                        if (currentBoard[i + 1, j + 1] == true)
+                        {
+                            alive++;
+                        }
+                    }
+                    catch { }
+
+                    try
+                    {
+                        if (currentBoard[i + 1, j] == true)
+                        {
+                            alive++;
+                        }
+                    }
+                    catch { }
+
+                    try
+                    {
+                        if (currentBoard[i + 1, j - 1] == true)
+                        {
+                            alive++;
+                        }
+                    }
+                    catch { }
+
+                    try
+                    {
+                        if (currentBoard[i, j - 1] == true)
+                        {
+                            alive++;
+                        }
+                    }
+                    catch { }
+
+
+                    if (currentBoard[i, j] == true)
+                    {
+                        if (alive < 2)
+                        {
+                            nextBoard[i, j] = false;
+                        }
+                        else if (alive < 4)
+                        {
+                            nextBoard[i, j] = true;
+                        }
+                        else
+                        {
+                            nextBoard[i, j] = false;
+                        }
+                    }
+                    else
+                    {
+                        if (alive == 3)
+                        {
+                            nextBoard[i, j] = true;
+                        }
+                        else
+                        {
+                            nextBoard[i, j] = false;
+                        }
+                    }
+                 }
+            }
+            currentBoard = nextBoard;
+        }
+
+
+        public bool[,] GetBoard()
+       {
+            return currentBoard;
+       }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConwaysGameOfLife;
+
+namespace Conways
+{
+    [TestClass]
+    public class GameOfLife
+    {
+        [TestMethod]
+        public void StillLife()
+        {
+            bool[,] StillLife = new bool[,] { { true, true }, { true, true } };
+            Map board = new Map(StillLife);
+            board.Tick();
+            CollectionAssert.AreEqual(StillLife, board.GetBoard());
+        }
+
+        [TestMethod]
+        public void Oscillator()
+        {
+            bool[,] oscillator = new bool[,] { { false, false, false }, { true, true, true }, { false, false, false } };
+            Map board = new Map(oscillator);
+            board.Tick();
+            bool[,] expected = new bool[,] { { false, true, false }, { false, true, false }, { false, true, false } };
+            CollectionAssert.AreEqual(expected, board.GetBoard());
+        }
+
+        [TestMethod]
+        public void StillLife2()
+        {
+            bool[,] StillLife2 = new bool[,] { { false, true, true, false }, { true, false, false, true }, { false, true, true, false } };
+            Map board = new Map(StillLife2);
+            board.Tick();
+            CollectionAssert.AreEqual(StillLife2, board.GetBoard());
+        }
+
+        [TestMethod]
+        public void Oscillator2()
+        {
+            bool[,] oscillator2 = new bool[,] { { false, false, false, false }, {false, true, true, true }, { true, true, true, false }, { false, false, false, false } };
+            Map board = new Map(oscillator2);
+            board.Tick();
+            bool[,] expected = new bool[,] { { false, false, true, false }, {true, false, false, true,  }, { true, false, false, true, }, { false, true, false, false } };
+            CollectionAssert.AreEqual(expected, board.GetBoard());
+        }
+
+        [TestMethod]
+        public void StillLife3()
+        {
+            bool[,] StillLife3 = new bool[,] { { false, true, true, false }, { true, false, false, true }, { false, true, false, true }, { false, false, true, false } };
+            Map board = new Map(StillLife3);
+            board.Tick();
+            CollectionAssert.AreEqual(StillLife3, board.GetBoard());
+        }
+
+        [TestMethod]
+        public void Oscillator3()
+        {
+            bool[,] oscillator3 = new bool[,] { { true, true, false, false }, { true, true, false, false }, { false, false, true, true }, { false, false, true, true } };
+            Map board = new Map(oscillator3);
+            board.Tick();
+            bool[,] expected = new bool[,] { { true, true, false, false }, { true, false, false, false }, { false, false, false, true }, { false, false, true, true } };
+            CollectionAssert.AreEqual(expected, board.GetBoard());
+        }
+
+        [TestMethod]
+        public void StillLife4()
+        {
+            bool[,] StillLife4 = new bool[,] { { true, true, false }, { true, false, true }, { false, true, false } };
+            Map board = new Map(StillLife4);
+            board.Tick();
+            CollectionAssert.AreEqual(StillLife4, board.GetBoard());
+        }
+    }
+}
